7f5937f6deca655f30a92d1be1f1dfd2c2729b16
ministerangebenit-dotcom <ministerangebenit@gmail.com>
2026-07-02 00:29:32 +0100

    Create MainLayout.jsx

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+export default function MainLayout() {
+
+return (
+
+<div className="min-h-screen bg-background text-white">
+
+<Outlet />
+
+</div>
+
+);
+
+}
