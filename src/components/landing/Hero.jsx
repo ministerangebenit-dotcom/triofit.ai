7b20e5573a7382b2f239d5a74fa9482a039612ca
ministerangebenit-dotcom <ministerangebenit@gmail.com>
2026-07-02 00:45:38 +0100

    Create Hero.jsx

---
+++ src/components/landing/Hero.jsx
@@ -0,0 +1,96 @@
+import { motion } from "framer-motion";
+
+export default function Hero() {
+  return (
+    <div className="text-center z-10">
+
+      <motion.img
+
+        src="/logo.png"
+
+        className="
+        w-24
+        h-24
+        rounded-3xl
+        mx-auto
+        shadow-2xl
+        "
+
+        initial={{
+          opacity:0,
+          scale:.7
+        }}
+
+        animate={{
+          opacity:1,
+          scale:1
+        }}
+
+        transition={{
+          duration:.8
+        }}
+
+      />
+
+      <motion.h1
+
+        className="
+        mt-10
+        text-6xl
+        font-display
+        tracking-[.25em]
+        "
+
+        initial={{
+          opacity:0,
+          y:25
+        }}
+
+        animate={{
+          opacity:1,
+          y:0
+        }}
+
+        transition={{
+          delay:.2
+        }}
+
+      >
+
+        TRIOFIT
+
+      </motion.h1>
+
+      <motion.p
+
+        className="
+        mt-8
+        max-w-xl
+        text-xl
+        text-gray-300
+        leading-9
+        mx-auto
+        "
+
+        initial={{
+          opacity:0
+        }}
+
+        animate={{
+          opacity:1
+        }}
+
+        transition={{
+          delay:.45
+        }}
+
+      >
+
+        Know exactly how you'll be perceived
+        before you walk into the room.
+
+      </motion.p>
+
+    </div>
+  );
+}
