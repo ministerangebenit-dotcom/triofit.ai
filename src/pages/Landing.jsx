89ad32a0e60da17af7f5186d7a203849c9692fca
ministerangebenit-dotcom <ministerangebenit@gmail.com>
2026-07-02 00:30:10 +0100

    Create Landing.jsx

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,17 @@
+export default function Landing() {
+
+return (
+
+<div className="h-screen flex items-center justify-center">
+
+<h1 className="text-5xl font-display">
+
+TRIOFIT
+
+</h1>
+
+</div>
+
+);
+
+}
