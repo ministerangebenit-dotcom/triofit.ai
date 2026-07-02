269dd4e4e7372cb4ccfcc436edf11bdee053b43b
ministerangebenit-dotcom <ministerangebenit@gmail.com>
2026-07-02 10:37:47 +0100

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,33 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-
 import MainLayout from "./layouts/MainLayout";
-
 import Landing from "./pages/Landing";
-
+import NameEntry from "./pages/NameEntry";
 import Conversation from "./pages/Conversation";
 
 export default function Router() {
-
-    return (
-
-        <Routes>
-
-            <Route element={<MainLayout />}>
-
-                <Route
-                    path="/"
-                    element={<Landing />}
-                />
-
-                <Route
-                    path="/conversation"
-                    element={<Conversation />}
-                />
-
-            </Route>
-
-        </Routes>
-
-    );
-
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Landing />} />
+        <Route path="/name" element={<NameEntry />} />
+        <Route path="/conversation" element={<Conversation />} />
+      </Route>
+    </Routes>
+  );
 }
