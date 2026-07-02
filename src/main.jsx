e6bff8f428ea456f3ce4ff83a23182b8ecd58bf3
ministerangebenit-dotcom <ministerangebenit@gmail.com>
2026-07-02 10:40:27 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
-
+import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-<React.StrictMode>
-
-<BrowserRouter>
-
-<App />
-
-</BrowserRouter>
-
-</React.StrictMode>
-
+  <React.StrictMode>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
 );
